7136878864aba61c5101fba03cd4cab1345a26e5
Josua Blejeru <37913833+josuablejeru@users.noreply.github.com>
2021-04-14 15:18:25 +0200

    cleanup the index page

--- frontend/betcoin/pages/index.tsx
+++ frontend/betcoin/pages/index.tsx
@@ -1,17 +1,16 @@
-import { Heading, Link, Text, Code } from "@chakra-ui/react";
+import { Heading, Link, VStack } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <>
+    <VStack>
       <Heading as="h1" size="2xl" mb="2">
-        Welcome to Next.js!
+        Welcome to Betcoin!
       </Heading>
       <Link color="teal.500" href="/game">
-        Start the game!!
+        Start the game here!!
       </Link>
-      <Text fontSize="xl" mt="2">
-        Get started by editing <Code>pages/index.js</Code>
-      </Text>
+      </VStack>
     </>
   );
 }
