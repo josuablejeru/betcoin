fbbb07d3bbec5d9f4247642623d325b449a265cd
Josua Blejeru <37913833+josuablejeru@users.noreply.github.com>
2021-04-16 22:59:05 +0200

    include dynamodb into sls

--- aws/ssm.tf
+++ aws/ssm.tf
@@ -7,3 +7,13 @@ resource "aws_ssm_parameter" "user_score_dynamodb_arn" {
     aws_dynamodb_table.user_score
   ]
 }
+
+resource "aws_ssm_parameter" "user_score_dynamodb" {
+  name = "/betcoin/${terraform.workspace}/user-score-dynamodb-name"
+  type = "String"
+  value = aws_dynamodb_table.user_score.id
+
+  depends_on = [
+    aws_dynamodb_table.user_score
+  ]
+}
