348093cbcbbd673358425509d8091e6d3d5cf79b
Josua Blejeru <37913833+josuablejeru@users.noreply.github.com>
2021-04-19 18:01:53 +0200

    inject variables in openapi template

--- aws/api_gateway.tf
+++ aws/api_gateway.tf
@@ -1,6 +1,10 @@
 resource "aws_api_gateway_rest_api" "openapi" {
   name        = "${terraform.workspace}-betcoin-api"
-  body        = file("../openapi.yaml")
+  body        = templatefile("../openapi.yaml", {
+    store_guess_function = "store-guess-${terraform.workspace}-store_guess",
+    region = var.aws_region,
+    account_id = data.aws_caller_identity.current.account_id
+    })
   description = "API Backend for Betcoin"
 
   endpoint_configuration {
@@ -24,4 +28,6 @@ resource "aws_api_gateway_stage" "backend" {
   deployment_id = aws_api_gateway_deployment.backend.id
   rest_api_id   = aws_api_gateway_rest_api.openapi.id
   stage_name    = terraform.workspace
+
+  
 }
