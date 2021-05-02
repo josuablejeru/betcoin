797f5a1623f8662efc82a94e4a54c757772ae391
Josua Blejeru <37913833+josuablejeru@users.noreply.github.com>
2021-05-03 00:06:06 +0200

    expand frontend tests

--- frontend/betcoin/tests/calculatePoint.test.ts
+++ frontend/betcoin/tests/calculatePoint.test.ts
@@ -1,13 +1,33 @@
 import { calculatePoint } from "../utils";
 
 describe("test Point calculation", () => {
-  it("should return -1", () => {
+  it("should return 1 then price is higher and bet was up", () => {
     const point = calculatePoint("UP", 11.0, 12.0);
     expect(point).toBe(1);
   });
 
-  it("it should return 1", () => {
+  it("should return 1 then price is lower and the bet was down", () => {
     const point = calculatePoint("DOWN", 10.0, 9.0);
+    expect(point).toBe(1);
+  });
+
+  it("should return -1 then the price is higher and the bet was down", () => {
+    const point = calculatePoint("DOWN", 10.0, 11.0);
+    expect(point).toBe(-1);
+  });
+
+  it("should return -1 then the price is lower and the bet was up", () => {
+    const point = calculatePoint("UP", 10.0, 9.0);
+    expect(point).toBe(-1);
+  });
+
+  it("should return -1 then the price is the same and the bet was up", () => {
+    const point = calculatePoint("UP", 10.0, 10.0);
+    expect(point).toBe(-1);
+  });
+
+  it("should return -1 then the price is the same and the bet was down", () => {
+    const point = calculatePoint("DOWN", 10.0, 10.0);
     expect(point).toBe(-1);
   });
 });
